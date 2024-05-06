67c96867a17abb9481bd32f8a7efc55a9f66fa46
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-05-06 12:55:55 +0200

    Improve HTTP error handling

--- CurrencyData.py
+++ CurrencyData.py
@@ -4,17 +4,16 @@ API_KEY = "7e50a8f6c133c6ce223a0d2b"
 class Currency():
     def  __init__(self, currencyCode):
         self.currencyCode = currencyCode
+        self.url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{self.currencyCode}"
     
     def getAllExchangeRates(self):
-        URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{self.currencyCode}"
-
-        response = requests.get(URL)
-        if response.status_code == 200:
-            data = response.json()
-            if data["result"] == "success":
-                return data["conversion_rates"]
-            else:
-                raise Exception(f"Failed to fetch data: {data['error']}")
+        response = requests.get(self.url)
+        response.raise_for_status()
+        data = response.json()
+        if data["result"] == "success":
+            return data["conversion_rates"]
+        else:
+            raise Exception(f"Failed to fetch data: {data['error']}")
 
 class ExchangeData():
     def __init__(self, currency1: Currency, currency2: Currency):
